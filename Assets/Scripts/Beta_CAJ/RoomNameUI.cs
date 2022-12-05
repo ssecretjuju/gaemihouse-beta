b3b782633b41f4a42004218bf9e79e6aeceedb3c
aeju <aejuchoe@naver.com>
2022-12-05 20:26:40 +0900

    Implement: "방 안에서 방이름 보이게 UI"

---
+++ Assets/Scripts/Beta_CAJ/RoomNameUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class RoomNameUI : MonoBehaviourPun
+{
+    public Text roomName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomName.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
